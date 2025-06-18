8111882da9583583db43b15f529493ec160def47
孙旺 <741967556@qq.com>
2025-06-18 08:58:54 +0800

    初始化

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1750168450169_6128',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
